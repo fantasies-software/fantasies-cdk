86e667f6dcf21770248e2907bc61302b4c57c290
cui <devcui@outlook.com>
2025-07-14 22:27:11 +0800

    chore: remove dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-export { a } from './Button/index.js';
-export { g as groupBy } from './utils/group-by.js';
-export { a as isArray, k as isDate, n as isEmpty, o as isEqual, j as isFloat, e as isFunction, h as isInt, c as isNil, g as isNumber, b as isObject, d as isPrimitive, m as isPromise, l as isRegexp, f as isString, i as isSymbol } from './utils/typed.js';
-export { z as zipAll } from './utils/zip.js';
-export { B as Button } from './Button/Button.js';
-//# sourceMappingURL=index.js.map
