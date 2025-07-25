e4116bfcf7aed71eb3b350c7aaff16507cf0124f
cui <devcui@outlook.com>
2025-07-25 22:09:24 +0800

    fix: fix in range override

--- src/utils/number/in-range.ts
+++ src/utils/number/in-range.ts
@@ -4,20 +4,18 @@
  *  eg: inRange(2, 1, 5) returns true because 2 is in the range [1, 5)
  */
 export function inRange(number: number, end: number): boolean
-export function inRange(number: number, start: number, end?: number): boolean {
-  const isTypeSafe
-    = typeof number === 'number'
-      && typeof start === 'number'
-      && (typeof end === 'undefined' || typeof end === 'number')
-
-  if (!isTypeSafe) {
+export function inRange(number: number, start: number, end: number): boolean
+export function inRange(number: number, startOrEnd: number, end?: number): boolean {
+  if (typeof number !== 'number' || typeof startOrEnd !== 'number' || (typeof end !== 'undefined' && typeof end !== 'number')) {
     return false
   }
-
+  let start: number
   if (typeof end === 'undefined') {
-    end = start
+    end = startOrEnd
     start = 0
   }
-
+  else {
+    start = startOrEnd
+  }
   return number >= Math.min(start, end) && number < Math.max(start, end)
 }
