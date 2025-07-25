8e76765f6ed27d5b8b33f43fc246cd227f2b1c90
cui <devcui@outlook.com>
2025-07-25 22:03:25 +0800

    chore: lint code

--- src/utils/number/to-float.ts
+++ src/utils/number/to-float.ts
@@ -2,19 +2,21 @@ export function toFloat<T extends number | null = number>(
   value: any,
   defaultValue?: T,
 ): number | T {
-  let def: number | T;
+  let def: number | T
   if (defaultValue === undefined) {
-    def = 0.0 as T;
-  } else {
-    def = defaultValue;
+    def = 0.0 as T
+  }
+  else {
+    def = defaultValue
   }
   if (value === null || value === undefined) {
-    return def;
+    return def
   }
-  const result = Number.parseFloat(value);
+  const result = Number.parseFloat(value)
   if (Number.isNaN(result)) {
-    return def;
-  } else {
-    return result as T;
+    return def
+  }
+  else {
+    return result as T
   }
 }
--- src/utils/number/to-int.ts
+++ src/utils/number/to-int.ts
@@ -2,19 +2,21 @@ export function toInt<T extends number | null = number>(
   value: any,
   defaultValue?: T,
 ): number | T {
-  let def;
+  let def
   if (defaultValue === undefined) {
-    def = 0;
-  } else {
-    def = defaultValue;
+    def = 0
+  }
+  else {
+    def = defaultValue
   }
   if (value === null || value === undefined) {
-    return def as number as T;
+    return def as number as T
   }
   const result = Number.parseInt(value)
   if (Number.isNaN(result)) {
-    return def as number as T;
-  } else {
-    return result as number as T;
+    return def as number as T
+  }
+  else {
+    return result as number as T
   }
 }
