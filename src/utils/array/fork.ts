5ebe993d8fddb296ad4fc43472fe6e91c6b0c4d2
cui <devcui@outlook.com>
2025-07-25 13:53:21 +0800

    feat: add fork func

---
+++ src/utils/array/fork.ts
@@ -0,0 +1,21 @@
+import { isArray } from "../typed";
+
+/**
+ *  Splits an array into two arrays based on a condition.
+ *  eg: `fork([1, 2, 3, 4], n => n % 2 === 0)` will return `[[2, 4], [1, 3]]`
+ */
+export function fork<T>(array: T[], condition: (item: T) => boolean): [T[], T[]] {
+  if (!isArray(array) || !array) {
+    return [[], []]
+  }
+  return array.reduce((acc, item) => {
+    const [a, b] = acc;
+    if (condition(item)) {
+      return [[...a, item], b];
+    } else {
+      return [a, [...b, item]];
+    }
+  },
+    [[], []] as [T[], T[]]
+  )
+}
